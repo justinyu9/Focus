aa9fa801eb87f51c0bb14779abb250e1d3faec3f
alhuang2 <alstonh323@gmail.com>
2017-11-16 00:28:41 -0800

    animations

--- app/src/main/java/cmps121/focus/MainActivity.java
+++ app/src/main/java/cmps121/focus/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,22 +21,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Animation translate = AnimationUtils.loadAnimation(this,R.anim.translate);
         final Animation translate2 = AnimationUtils.loadAnimation(this,R.anim.translate2);
 
         Button myButton = (Button) findViewById(R.id.button);
         myButton.setOnClickListener(new android.view.View.OnClickListener() {
             @Override
             public void onClick(android.view.View view) {
-                view.startAnimation(translate);
+                //view.startAnimation(translate);
                 view.startAnimation(translate2);
+                CountDownTimer countDownTimer = new CountDownTimer(850, 1000) {
+                    @Override
+                    public void onTick(long l) {
 
-                Intent intent = new Intent(MainActivity.this, Tasks.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                //intent.putExtra("Text 2", edit_text_2.getText().toString());
-                Bundle bundle = new Bundle();
-                intent.putExtras(bundle);
-                startActivity(intent);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        Intent intent = new Intent(MainActivity.this, Tasks.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        //intent.putExtra("Text 2", edit_text_2.getText().toString());
+                        Bundle bundle = new Bundle();
+                        intent.putExtras(bundle);
+                        startActivity(intent);
+                    }
+                }.start();
         }
         });
     }
